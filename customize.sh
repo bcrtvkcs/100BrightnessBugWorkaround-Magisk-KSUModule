58e073d82cbb9017be4f879b0e16c714af641207
K. Cihan Sipahi <32391714+bcrtvkcs@users.noreply.github.com>
2026-03-25 15:48:28 +0300

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -17,6 +17,7 @@ ui_print ""
 # Set permissions
 set_perm_recursive $MODPATH 0 0 0755 0644
 set_perm $MODPATH/service.sh 0 0 0755
+set_perm $MODPATH/action.sh 0 0 0755
 
 ui_print ""
 ui_print "- Initialization completed!"
